84dc3466666068f85444bebd584ccbc36f25a212
Kseniya G Vo <vwk8888@gmail.com>
2020-02-07 20:05:10 +0200

    Updates

---
+++ precache-manifest.556f1de99b905047acb33b2495c0ca01.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4cfa4e68b5c33311ffa267d52401c32f",
+    "url": "/goit-react-hw-02-phonebook/index.html"
+  },
+  {
+    "revision": "113a6e542898a2966f15",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.2f456e35.chunk.js"
+  },
+  {
+    "revision": "00d09ea4a4fa9d8a0ab09b2f6388ac4c",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.2f456e35.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5226bcd7b21fbabafa47",
+    "url": "/goit-react-hw-02-phonebook/static/js/main.adf06254.chunk.js"
+  },
+  {
+    "revision": "5eea2426daa3643f03a9",
+    "url": "/goit-react-hw-02-phonebook/static/js/runtime-main.6819ab10.js"
+  }
+]);
